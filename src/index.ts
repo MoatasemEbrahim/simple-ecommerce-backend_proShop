ca692d0215aaefc669755488beb72b4859f3b1ec
moatasem.ebrahim <moatasem.ebrahim@fixedmea.com>
2020-11-16 01:28:02 +0200

    main server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world!')
+import express from 'express'
+// const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('API is running ...')
+})
+
+app.listen(5000,()=>{console.log('Server has been started')})
